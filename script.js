579b8592d50f2bd4872e51153edd1db35ea9242e
unknown <ramachandranmuthusamy7@gmail.com>
2026-03-19 23:45:05 +0530

    hgdtuygu

---
+++ script.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => {
+  link.addEventListener("click", function () {
+    links.forEach(l => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+// Highlight active section on scroll
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  document.querySelectorAll(".section").forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  links.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
